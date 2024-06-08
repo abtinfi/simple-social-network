5c6d94fc2f8dd4f9db9393d7e154442946639e6b
Abtin <abtinfiroozyar@gmail.com>
2024-06-08 22:26:55 +0330

    RegisterView

--- account/views.py
+++ account/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 
 class RegisterView(View):
+    form_class = UserRegisterForm
+    template_form = "account/register.html"
+
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, "account/register.html", {"form": form})
+        form = self.form_class()
+
+        return render(request, self.template_form, {"form": form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(
+                username=cd["username"], email=cd["email"], password=cd["password"]
+            )
+            messages.success(request, "you registered successfuly", "success")
+            return redirect("home:home")
+        return render(request, self.template_form, {"form": form})
--- social_network/urls.py
+++ social_network/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls")),
-    path("accounts/", include("account.urls")),
+    path("", include("home.urls", namespace="home")),
+    path("accounts/", include("account.urls", namespace="account")),
 ]
