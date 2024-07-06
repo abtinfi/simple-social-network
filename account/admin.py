bad30a9c286942c63dfb7888503c5404c1d58fff
Abtin Firoozyar <abtinfiroozyar@gmail.com>
2024-07-06 13:04:11 +0330

    \

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user", "slug", "updated"]
+    search_fields = ["slug", "body"]
+    list_filter = ["updated"]
+
+
+admin.site.register(Post, PostAdmin)
